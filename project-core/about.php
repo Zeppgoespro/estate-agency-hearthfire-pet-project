bd43916add48ea00fa089137d56435c9b4d59f0b
Zepp <zeppgoesblack@gmail.com>
2023-02-13 22:48:18 +0500

    saved.php is ready + about us under construction

--- project-core/about.php
+++ project-core/about.php
@@ -33,6 +33,59 @@
   <!-- header section ends -->
 
 
+  <!-- about us section starts -->
+
+    <section class="about">
+
+      <div class="row">
+        <div class="image">
+          <img src="./images/about-img.svg">
+        </div>
+        <div class="content">
+          <h3>why choose us?</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis molestiae debitis facilis eum in perferendis error nam repellendus, quam maiores. Voluptas quisquam numquam sapiente quas.</p>
+          <a href="./contact.php" class="inline-btn">contact us</a>
+        </div>
+      </div>
+
+    </section>
+
+  <!-- about us section ends -->
+
+
+  <!-- steps section starts -->
+
+    <section class="steps">
+
+      <h1 class="heading">3 simple steps</h1>
+
+      <div class="box-container">
+
+        <div class="box">
+          <img src="./images/step-1.png">
+          <h3>search property</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, dolores.</p>
+        </div>
+
+        <div class="box">
+          <img src="./images/step-2.png">
+          <h3>contact dealer</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, dolores.</p>
+        </div>
+
+        <div class="box">
+          <img src="./images/step-3.png">
+          <h3>enjoy property</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, dolores.</p>
+        </div>
+
+      </div>
+
+    </section>
+
+  <!-- steps section ends -->
+
+
   <!-- footer section starts -->
 
   <?php include './components/footer.php' ?>
