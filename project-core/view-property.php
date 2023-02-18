a46fb416583be9c2fa4a0a261804e879da2fde4c
Zepp <zeppgoesblack@gmail.com>
2023-02-18 23:00:58 +0500

    view-property.php is going on

--- project-core/view-property.php
+++ project-core/view-property.php
@@ -65,8 +65,7 @@
             $fetch_user = $select_user->fetch(PDO::FETCH_ASSOC);
 
             $select_saved = $conn->prepare("SELECT * FROM `saved` WHERE property_id = ? AND user_id = ?");
-            $select_saved->execute([$property_id, $user_id]);
-
+            $select_saved->execute([$property_id, $user_id]); # need to control
       ?>
 
       <div class="details">
@@ -77,28 +76,36 @@
             <?php
             switch (!empty($fetch_property['image_2'])):
               case true:
-                echo '<img src="./uploaded-files/'.$fetch_property['image_2'].'" alt="Property image" class="swiper-slide">';
+            ?>
+            <img src="./uploaded-files/<?= $fetch_property['image_2'] ?>" alt="Property image" class="swiper-slide">;
+            <?php
               break;
             endswitch;
             ?>
             <?php
             switch (!empty($fetch_property['image_3'])):
               case true:
-                echo '<img src="./uploaded-files/'.$fetch_property['image_3'].'" alt="Property image" class="swiper-slide">';
+            ?>
+            <img src="./uploaded-files/<?= $fetch_property['image_3'] ?>" alt="Property image" class="swiper-slide">;
+            <?php
               break;
             endswitch;
             ?>
             <?php
             switch (!empty($fetch_property['image_4'])):
               case true:
-                echo '<img src="./uploaded-files/'.$fetch_property['image_4'].'" alt="Property image" class="swiper-slide">';
+            ?>
+            <img src="./uploaded-files/<?= $fetch_property['image_4'] ?>" alt="Property image" class="swiper-slide">;
+            <?php
               break;
             endswitch;
             ?>
             <?php
             switch (!empty($fetch_property['image_5'])):
               case true:
-                echo '<img src="./uploaded-files/'.$fetch_property['image_5'].'" alt="Property image" class="swiper-slide">';
+            ?>
+            <img src="./uploaded-files/<?= $fetch_property['image_5'] ?>" alt="Property image" class="swiper-slide">;
+            <?php
               break;
             endswitch;
             ?>
@@ -106,10 +113,81 @@
           <div class="swiper-pagination"></div>
         </div>
 
+        <h3 class="name"><?= $fetch_property['property_name'] ?></h3>
+        <p class="address"><i class="fas fa-map-marker-alt"></i><span><?= $fetch_property['address'] ?></span></p>
+        <div class="info">
+
+          <p><i class="fa-solid fa-dollar-sign"></i><span><?= $fetch_property['price'] ?></span></p>
+          <p><i class="fas fa-user"></i><span><?= $fetch_user['name'] ?></span></p>
+          <p><i class="fas fa-phone"></i><a href="tel:<?= $fetch_user['number'] ?>"><?= $fetch_user['number'] ?></a></p>
+          <p><i class="fas fa-building"></i><span><?= $fetch_property['offer'] ?></span></p>
+          <p><i class="fas fa-house"></i><span><?= $fetch_property['type'] ?></span></p>
+          <p><i class="fas fa-calendar"></i><span><?= $fetch_property['date'] ?></span></p>
+
+        </div>
+
+        <h3 class="title">details</h3>
+        <div class="flex">
+
+          <div class="box">
+            <p><i>bhk:</i><span><?= $fetch_property['bhk'] ?> BHK</span></p>
+            <p><i>deposit amount:</i><span><span class="fa-solid fa-dollar-sign" style="margin-right: .5rem;"></span><?= $fetch_property['deposite'] ?></span></p>
+            <p><i>status:</i><span><?= $fetch_property['status'] ?></span></p>
+            <p><i>bedroom:</i><span><?= $fetch_property['bedroom'] ?></span></p>
+            <p><i>bathroom:</i><span><?= $fetch_property['bathroom'] ?></span></p>
+            <p><i>balcony:</i><span><?= $fetch_property['balcony'] ?></span></p>
+          </div>
+          <div class="box">
+            <p><i>carpet area:</i><span><?= $fetch_property['carpet'] ?>sqft</span></p>
+            <p><i>age:</i><span><?= $fetch_property['age'] ?> years</span></p>
+            <p><i>total floors:</i><span><?= $fetch_property['total_floors'] ?></span></p>
+            <p><i>rooms per floor:</i><span><?= $fetch_property['room_floor'] ?></span></p>
+            <p><i>furniture:</i><span><?= $fetch_property['furnished'] ?></span></p>
+            <p><i>loan:</i><span><?= $fetch_property['loan'] ?></span></p>
+          </div>
+
+          <h3 class="title">amenities</h3>
+          <div class="flex">
+
+            <div class="box">
+              <p><i class="fas fa-<?php if($fetch_property['lift'] == 'yes'){echo 'check';}else{echo 'times';} ?>"></i><span>lift</span></p>
+              <p><i class="fas fa-<?php if($fetch_property['security_guard'] == 'yes'){echo 'check';}else{echo 'times';} ?>"></i><span>security guard</span></p>
+              <p><i class="fas fa-<?php if($fetch_property['play_ground'] == 'yes'){echo 'check';}else{echo 'times';} ?>"></i><span>play ground</span></p>
+              <p><i class="fas fa-<?php if($fetch_property['garden'] == 'yes'){echo 'check';}else{echo 'times';} ?>"></i><span>garden</span></p>
+              <p><i class="fas fa-<?php if($fetch_property['water_supply'] == 'yes'){echo 'check';}else{echo 'times';} ?>"></i><span>water supply</span></p>
+              <p><i class="fas fa-<?php if($fetch_property['power_backup'] == 'yes'){echo 'check';}else{echo 'times';} ?>"></i><span>power backup</span></p>
+            </div>
+            <div class="box">
+              <p><i class="fas fa-<?php if($fetch_property['parking_area'] == 'yes'){echo 'check';}else{echo 'times';} ?>"></i><span>parking area</span></p>
+              <p><i class="fas fa-<?php if($fetch_property['gym'] == 'yes'){echo 'check';}else{echo 'times';} ?>"></i><span>gym</span></p>
+              <p><i class="fas fa-<?php if($fetch_property['shopping_mall'] == 'yes'){echo 'check';}else{echo 'times';} ?>"></i><span>shopping mall</span></p>
+              <p><i class="fas fa-<?php if($fetch_property['hospital'] == 'yes'){echo 'check';}else{echo 'times';} ?>"></i><span>hospital</span></p>
+              <p><i class="fas fa-<?php if($fetch_property['school'] == 'yes'){echo 'check';}else{echo 'times';} ?>"></i><span>school</span></p>
+              <p><i class="fas fa-<?php if($fetch_property['market_area'] == 'yes'){echo 'check';}else{echo 'times';} ?>"></i><span>market area</span></p>
+            </div>
+
+          </div>
+
+          <h3 class="title">description</h3>
+          <p class="description"><?= $fetch_property['description'] ?></p>
+
+          <form action="" method="post" class="flex-btn">
+
+            <input type="hidden" name="property_id" value="<?= $property_id ?>">
+            <?php if ($select_saved->rowCount() > 0): ?>
+              <button type="submit" name="save" class="save"><i class="fas fa-heart"></i><span>saved</span></button>
+            <?php else: ?>
+              <button type="submit" name="save" class="save"><i class="far fa-heart"></i><span>save</span></button>
+            <?php endif; ?>
+            <input type="submit" value="send request" name="send" class="btn">
+
+          </form>
+
+        </div>
+
       </div>
 
       <?php
-
           endwhile;
         else:
           echo '<p class="empty">property not found</p>';
@@ -122,17 +200,19 @@
   <!-- view section ends -->
 
 
+  <!-- swiper script -->
+  <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
+
+  <!-- sweetalert cdn link -->
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+
   <!-- footer section starts -->
 
   <?php include './components/footer.php' ?>
 
   <!-- footer section ends -->
 
-  <!-- swiper script -->
-  <script src="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js"></script>
-
-  <!-- sweetalert cdn link -->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
 
   <!-- custom js file link -->
   <script src="./scripts/script.js"></script>
@@ -140,22 +220,25 @@
   <?php include './components/messages.php' ?>
 
   <script>
-    var swiper = new Swiper(".images-container", {
-      loop: true,
+
+    let swiper = new Swiper(".images-container", {
       effect: "coverflow",
       grabCursor: true,
+      centeredSlides: true,
       slidesPerView: "auto",
+      loop:true,
       coverflowEffect: {
         rotate: 0,
         stretch: 0,
         depth: 200,
-        modifier: 2,
+        modifier: 3,
         slideShadows: true,
       },
       pagination: {
         el: ".swiper-pagination",
       },
     });
+
   </script>
 
 </body>
