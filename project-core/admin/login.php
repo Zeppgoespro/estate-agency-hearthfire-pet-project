77a4bf065badee34bb91b40ebcccc9ba6c8181dd
Zepp <zeppgoesblack@gmail.com>
2023-03-08 23:00:32 +0500

    admin part - login page

--- project-core/admin/login.php
+++ project-core/admin/login.php
@@ -2,6 +2,19 @@
 
   include '../components/connect.php';
 
+  if (isset($_POST['submit'])):
+
+    $name = $_POST['name'];
+    $pass = $_POST['pass'];
+
+    $verify_admin = $conn->prepare("SELECT * FROM `admins` WHERE name = ? AND password = ? LIMIT 1");
+    $verify_admin->execute([$name, $pass]);
+    $row = $verify_admin->fetch(PDO::FETCH_ASSOC);
+
+
+
+  endif;
+
 ?>
 
 <!DOCTYPE html>
@@ -32,7 +45,15 @@
 
   <section class="form-container">
 
-    <form action=""></form>
+    <form action="" method="post">
+
+      <h3>welcome back</h3>
+      <p>default password = <span>999</span> | | default name = <span>admin</span></p>
+
+      <input type="text" class="box" name="name" required placeholder="enter your name" maxlength="20" oninput="this.value.replace(/\s/g, '')">
+      <input type="password" class="box" name="pass" required placeholder="enter your password" maxlength="20" oninput="this.value.replace(/\s/g, '')">
+      <input type="submit" value="login" name="submit" class="btn">
+    </form>
 
   </section>
 
