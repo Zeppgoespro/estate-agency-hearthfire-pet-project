ea091acbe8ad2f07f065c51618044200527ab733
Zepp <zeppgoesblack@gmail.com>
2023-03-14 22:54:42 +0500

    admin listings section

--- project-core/admin/listings.php
+++ project-core/admin/listings.php
@@ -42,7 +42,7 @@
 
       <h1 class="heading"> properties listed</h1>
 
-      <form action="" method="post">
+      <form action="" method="post" class="search-form">
 
         <input type="text" name="search_box" placeholder="search listings" maxlength="100">
         <button type="submit" name="search_btn" class="fas fa-search"></button>
@@ -58,6 +58,86 @@
             $search_box = $_POST['search_box'];
             $select_listings = $conn->prepare("SELECT * FROM `properties` WHERE name LIKE '%{$search_box}%' OR WHERE address LIKE '%{$search_box}%' ORDER BY date DESC");
             $select_listings->execute();
+
+          else:
+
+            $select_listings = $conn->prepare("SELECT * FROM `properties` ORDER BY date DESC");
+            $select_listings->execute();
+
+          endif;
+
+          if ($select_listings->rowCount() > 0):
+
+            while ($fetch_property = $select_listings->fetch(PDO::FETCH_ASSOC)):
+
+              $property_id = $fetch_property['id'];
+
+              $select_users = $conn->prepare("SELECT * FROM `users` WHERE id = ?");
+              $select_users->execute([$fetch_property['user_id']]);
+              $fetch_users = $select_users->fetch(PDO::FETCH_ASSOC);
+
+              if (!empty($fetch_property['image_2'])):
+                $count_image_2 = 1;
+              else:
+                $count_image_2 = 0;
+              endif;
+
+              if (!empty($fetch_property['image_3'])):
+                $count_image_3 = 1;
+              else:
+                $count_image_3 = 0;
+              endif;
+
+              if (!empty($fetch_property['image_4'])):
+                $count_image_4 = 1;
+              else:
+                $count_image_4 = 0;
+              endif;
+
+              if (!empty($fetch_property['image_5'])):
+                $count_image_5 = 1;
+              else:
+                $count_image_5 = 0;
+              endif;
+
+              $total_images = (1 + $count_image_2 + $count_image_3 + $count_image_4 + $count_image_5);
+
+        ?>
+
+        <div class="box">
+
+          <div class="thumb">
+
+            <p><i class="fas fa-image"></i><span><?= $total_images ?></span></p>
+            <img src="../uploaded-files/<?= $fetch_property['image_1'] ?>" alt="Estate photo">
+
+          </div>
+
+          <p class="price"><i class="fa-solid fa-dollar-sign"></i><span><?= $fetch_property['price'] ?></span></p>
+          <h3><?= $fetch_property['property_name'] ?></h3>
+          <p class="address"><i class="fas fa-map-marker-alt"></i><span><?= $fetch_property['address'] ?></span></p>
+
+          <form action="" method="post" class="flex-btn">
+
+            <input type="hidden" name="delete_id" value="<?= $property_id ?>">
+            <a href="./view-property.php?get_id=<?= $property_id ?>" class="btn">view</a>
+            <input type="submit" value="delete" name="delete" class="delete-btn" onclick="return confirm('delete this property?');">
+
+          </form>
+
+        </div>
+
+        <?php
+
+            endwhile;
+
+          elseif (isset($_POST['search_box']) || isset($_POST['search_btn'])):
+
+            echo '<p class="empty">no results found</p>';
+
+          else:
+
+            echo '<p class="empty">no properties listed yet</p>';
 
           endif;
 
